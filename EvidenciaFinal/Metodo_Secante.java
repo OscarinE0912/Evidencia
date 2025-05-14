e98404b8ecaa17656e4da637d1819b5729d12869
Oscar0912 <Oscarineduardorivera@gmail.com>
2025-05-14 12:41:23 -0700

    Add files via upload

--- EvidenciaFinal/Metodo_Secante.java
+++ EvidenciaFinal/Metodo_Secante.java
@@ -7,7 +7,7 @@ public class Metodo_Secante {
         Scanner scanner = new Scanner(System.in);
         //Variables Iniciales
         int iteracion = 0;
-        double xa = 0, xb = 0, xr, Ea=100;
+        double xa = 0, xb = 0, xr = 0, Ea=100, fxr_fxa = 0;
 
         //Pedir datos iniciales
         System.out.println("Ingresa el xa:");
@@ -17,18 +17,36 @@ public class Metodo_Secante {
 
         //Iniciar Iteración
         do{
+            //Condicional para cambiar xa o xb
+            if (iteracion != 0){
+                if(fxr_fxa < 0){
+                    xa=xr;
+                } else{
+                    xb=xr;
+                }
 
+            }
+
+            //Obtener el resultado de xr
             xr = (xa+xb)/2;
             System.out.println(xr);
 
-            iteracion=+1;
+            //Hacer la funcion de xa
+            double fxa = Funcion(xa);
+
+            //Hacer la funcion de xr
+            double fxr = Funcion(xr);
+
+            //Hacer la multiplicacion de funciones
+            fxr_fxa = fxa*fxr;
+
+            iteracion+=1;
         }while(Ea<1);
 
     }
 
     //Función del problema
-    public double Funcion(double num){
-        Double resultado = Math.pow(num,3)-(4*num)-9;
-        return resultado;
+    public static double Funcion(double num){
+        return Math.pow(num, 3) - (4 * num) - 9;
     }
 }
